6874bb6bb56bcbe4136c7df2418780076f02d13c
LuluOrosB <maria@bisonic.io>
2023-02-27 15:05:38 -0600

    ADD 2512512çdd

--- main.py
+++ main.py
@@ -132,9 +132,9 @@ dict_f['Vol'] = dict_elem1.values()
 dframe = pd.DataFrame.from_dict(dict_f)
 dframe2=dframe[dframe['Vol']>0]
 #dframe2['%'] = dframe2['Vol']/sum(dframe2)
-#st.dataframe(dframe2)
+st.dataframe(dframe2)
 
-fig11 = px.pie(dframe2, values='Vol', names='Materials')
-st.plotly_chart(fig11)
+#fig11 = px.pie(dframe2, values='Vol', names='Materials')
+#st.plotly_chart(fig11)
 
 
